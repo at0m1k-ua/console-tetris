9e5b1ead75f447f2c61d8ee91069188c371dfbf4
Nicky <kolianpylupc@gmail.com>
2022-05-15 10:45:59 +0300

    Commit #8: added exeption handling for memory allocation

--- src/GameField.cpp
+++ src/GameField.cpp
@@ -1,10 +1,21 @@
 #include "GameField.h"
+#include <new>
+#include <iostream>
 
-GameField::GameField(int gf_size_x, int gf_size_y) {
+GameField::GameField(int gf_size_x, int gf_size_y)
+{
     this->gf_size_x = gf_size_x;
     this->gf_size_y = gf_size_y;
-    field = new int*[gf_size_y];
-    for(int i = 0; i < gf_size_y; i++)
+    try
+    {
+        field = new int *[gf_size_y];
+    }
+    catch (std::bad_alloc xa)
+    {
+        std::cout << "Allocation failure.\n";
+        return;
+    }
+    for (int i = 0; i < gf_size_y; i++)
         field[i] = new int[gf_size_x]{0};
 }
 void GameField::obj_fall()
@@ -13,4 +24,3 @@ void GameField::obj_fall()
 void GameField::obj_stop()
 {
 }
-
