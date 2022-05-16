865f2b02dbc02c87a1c2fe14ab0eded2b07de14f
nicky <kolianpylupc@gmail.com>
2022-05-16 21:05:46 +0300

    Commit #13: added class ActiveShape

---
+++ src/ActiveShape.cpp
@@ -0,0 +1,31 @@
+#include "ActiveShape.h"
+
+ActiveShape::ActiveShape(GameField* m_gm, Gui* m_gui, Shape *shape, int m_color, int m_x)
+    : gamefield(m_gm), gui(m_gui), StatesList(shape->getCurrentState()), color(m_color), x(m_x)
+{
+    y = 0;
+}
+
+int ActiveShape::getColor() {
+    return color;
+}
+
+void ActiveShape::moveDown() {
+
+}
+
+void ActiveShape::moveLeft() {
+    
+}
+
+void ActiveShape::moveRight() {
+    
+}
+
+void ActiveShape::setNextState() {
+
+}
+
+bool ActiveShape::touchesGameFieldState() {
+    
+}
---
+++ src/ActiveShape.h
@@ -0,0 +1,27 @@
+#pragma once
+#ifndef _ACTIVESHAPE_H_
+#define _ACTIVESHAPE_H_
+#include "Gui.h"
+#include "Shape.h"
+#include "GameField.h"
+#include "ShapeLoader.h"
+
+class ActiveShape {
+    int x;
+    int y;
+    int color;
+    ShapeState* StatesList;
+    Gui* gui;
+    GameField* gamefield;
+
+    public:
+    ActiveShape(GameField* m_gm, Gui* m_gui, Shape *shape, int m_color, int m_x);
+    int getColor();
+    void moveLeft();
+    void moveRight();
+    void moveDown();
+    void setNextState();
+    bool touchesGameFieldState();
+};
+
+#endif
